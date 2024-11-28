265ffebb810be682acd19a83f555727dcfc0d354
idy <idy@wave.com>
2024-11-28 14:34:01 +0000

    adding save fonction and data file

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+
+
+    with open("credential.txt", "a") as credential:
+        credential.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -14,7 +24,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image = logo_img)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=1)
 
 #labels
 website_label=Label(text="Website:")
@@ -35,7 +45,7 @@ password_entry.grid(row=3, column=1)
 #Button
 generate_password_button = Button(text="Generate Password", width=15)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=32)
+add_button = Button(text="Add", width=32, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
