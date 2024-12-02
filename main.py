3bd48d0919353b0e6d9b61d3fd59f47261c3948f
idy <idy@wave.com>
2024-12-02 21:38:44 +0000

    adding json and exception

--- main.py
+++ main.py
@@ -1,20 +1,68 @@
 from symbol import with_item
 from tkinter import *
+from tkinter import messagebox
+from random import randint,choice,shuffle
+import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+    password_letters = [choice(letters) for _ in range(randint(8,10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
 
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="any field can't be empty")
 
-    with open("credential.txt", "a") as credential:
-        credential.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
+    else:
+
+        # is_ok = messagebox.askokcancel(title=website, message=f"these are the details entered: \nEmail: {email}"
+        #                        f"\nPassword: {password} \n tape OK to Save or Cancel to edit again")
+        # if is_ok:
+        try:
+            with open("credential.json", "r") as credential:
+                # Reading data with json
+                data = json.load(credential)
+        except FileNotFoundError:
+            with open(credential.json, "w") as credential:
+                json.dump(new_data, credential, indent=4)
+        else:
+            #Updating old data
+            data.update(new_data)
+            with open("credential.json", "w") as credential:
+                #saving data
+                json.dump(data, credential, indent=4)
+        finally:
+
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -43,9 +91,9 @@ email_username_entry.insert(0, "idy.ndoye@gmail.com")
 password_entry = Entry(width=16)
 password_entry.grid(row=3, column=1)
 #Button
-generate_password_button = Button(text="Generate Password", width=15)
+generate_password_button = Button(text="Generate Password", width=15, command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=32, command=save)
+add_button = Button(text="Add", width=33, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
